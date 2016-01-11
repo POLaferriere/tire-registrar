023f8aac1f2d2aa1fa3d48e703f81df6e7f3065c
Pierre Laferriere <polaferriere@gmail.com>
2016-01-11 14:09:22 -0500

    Initial commit

---
+++ app/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+var App = React.createClass({
+
+  render() {
+    return (
+      <h1>App</h1>
+    );
+  }
+
+});
+
+export default App;
---
+++ app/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import Index from './components/index';
+
+ReactDOM.render((
+	<Router>
+		<Route path='/' component={App}/>
+	</Router>
+), document.getElementById('application'));
